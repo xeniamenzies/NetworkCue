a59411a62f228f1bce90675e5c6a76c10407bb07
gywst <gywst@yahoo.com>
2013-05-25 10:36:04 +0300

    verstion 0.12

--- src/com/gw/networkcue/MainActivity.java
+++ src/com/gw/networkcue/MainActivity.java
@@ -2,7 +2,11 @@ package com.gw.networkcue;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,8 +14,38 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
+		final EditText edittextEmailAddress = (EditText)findViewById(R.id.etEmail);
+		// final EditText edittextEmailSubject = (EditText)findViewById(R.id.email_subject);
+		// final EditText edittextEmailText = (EditText)findViewById(R.id.email_text);
+
+		final String edittextEmailSubject = "Email Subject" ;
+		final String edittextEmailText = "Email Text";
+
+		Button buttonSendEmail_intent = (Button)findViewById(R.id.btnEmail);
+
+		buttonSendEmail_intent.setOnClickListener(new Button.OnClickListener(){
+
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+
+				String emailAddress = edittextEmailAddress.getText().toString();
+				//	String emailSubject = edittextEmailSubject.getText().toString();
+				//	String emailText = edittextEmailText.getText().toString();
+				String emailSubject = edittextEmailSubject;
+				String emailText = edittextEmailText;
+
+				String emailAddressList[] = {emailAddress};
+
+				Intent intent = new Intent(Intent.ACTION_SEND); 
+				intent.setType("plain/text");
+				intent.putExtra(Intent.EXTRA_EMAIL, emailAddressList);  
+				intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject); 
+				intent.putExtra(Intent.EXTRA_TEXT, emailText); 
+				startActivity(Intent.createChooser(intent, "Choice App to send email:"));
+
+			}});
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
